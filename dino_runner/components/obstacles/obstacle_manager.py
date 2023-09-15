91b55c8ae613f982897c27ef17daa789bd0c7ec6
MurdokZerah <gabriel.lg01092006@gmail.com>
2023-09-14 22:20:39 -0300

    codigo da aula
    O que é o que é tem 50 pernas e não anda
    
    resposta 25 cadeirantes

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,28 +1,34 @@
 import pygame
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
-from dino_runner.components.obstacles.Bird import Pterodactil
-from dino_runner.utils.constants import BIRD
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
-    
+
     def update(self, game):
+        obstacle_type = [
+            Cactus(),
+            Bird(),
+        ]
+
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-            self.obstacles.append(Pterodactil(BIRD))
+            self.obstacles.append(obstacle_type[random.randint(0,1)])
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(500)
-                game.playing = False
-                break
-
+               pygame.time.delay(500)
+               game.playing = False
+               game.death_count += 1
+               break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
