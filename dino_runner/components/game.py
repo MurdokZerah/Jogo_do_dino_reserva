038f712495de86843bef0a054d1dcd44a0d2d521
MurdokZerah <gabriel.lg01092006@gmail.com>
2023-09-15 21:46:57 -0300

    tela de restart junto com o death count e o high score + reset speed e score
    se era pra botar uma tela diferenciada eu não sabia se esse for o caso quando for o dia da aula me fala e eu crio

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,11 +1,10 @@
+import random
 import pygame
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 FONT_STYLE = "freesansbold.ttf"
-
 
 class Game:
     def __init__(self):
@@ -33,7 +32,6 @@ class Game:
         pygame.quit()
 
     def run(self):
-        # Game loop: events - update - draw
         self.playing = True
         self.obstacle_manager.reset_obstacles()
         while self.playing:
@@ -59,12 +57,11 @@ class Game:
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255)) # "#FFFFFF"
+        self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
         self.draw_score()
-        #pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
@@ -87,27 +84,38 @@ class Game:
         self.screen.fill((255, 255, 255))
         half_screen_height = SCREEN_HEIGHT // 2
         half_screen_width = SCREEN_WIDTH // 2
-        
+
         if self.death_count == 0:
             font = pygame.font.Font(FONT_STYLE, 22)
             text = font.render("Press any key to start", True, (0, 0, 0))
             text_rect = text.get_rect()
-            text_rect.center =  (half_screen_width, half_screen_height)
+            text_rect.center = (half_screen_width, half_screen_height)
             self.screen.blit(text, text_rect)
         else:
             self.screen.blit(ICON, (half_screen_width - 20, half_screen_height - 40))
-            # TAREFA: CRIAR TELA DE RESTART
-            #  CRIAR MÉTODO PARA REMOVER REPETIÇÃO DE CÓDIGO PARA ESCREVER TEXTO
-            
-            # Escrever "Press any key to restart"
-            # Escrever o acumulado de death_count
-            # Escrever o Score atingido naquela partida
 
-            ## RESETAR:
-            ##  score
-            ##  game_speed
+            # Reinicio
+            restart_font = pygame.font.Font(FONT_STYLE, 22)
+            restart_text = restart_font.render("Press any key to restart", True, (0, 0, 0))
+            restart_text_rect = restart_text.get_rect()
+            restart_text_rect.center = (half_screen_width, half_screen_height + 40)
+            self.screen.blit(restart_text, restart_text_rect)
 
-        pygame.display.update() # .flip()
+            # Contagem de mortes.
+            death_count_font = pygame.font.Font(FONT_STYLE, 22)
+            death_count_text = death_count_font.render(f"Death Count: {self.death_count}", True, (0, 0, 0))
+            death_count_text_rect = death_count_text.get_rect()
+            death_count_text_rect.center = (half_screen_width, half_screen_height + 80)
+            self.screen.blit(death_count_text, death_count_text_rect)
+
+            # High Score.
+            score_font = pygame.font.Font(FONT_STYLE, 22)
+            score_text = score_font.render(f"Previous Score: {self.score}", True, (0, 0, 0))
+            score_text_rect = score_text.get_rect()
+            score_text_rect.center = (half_screen_width, half_screen_height + 120)
+            self.screen.blit(score_text, score_text_rect)
+
+        pygame.display.update()
         self.handle_events_on_menu()
 
     def handle_events_on_menu(self):
@@ -116,4 +124,10 @@ class Game:
                 self.playing = False
                 self.running = False
             elif event.type == pygame.KEYDOWN:
+                self.score = 0
+                self.game_speed = 20
                 self.run()
+
+if __name__ == "__main__":
+    game = Game()
+    game.execute()
